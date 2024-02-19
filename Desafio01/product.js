3a1adaf71d79765ee1f3cb6a1d1d7b43415dc734
Dario Argüello <arguellodario1@gmail.com>
2024-02-19 10:59:20 -0300

    first commit

---
+++ Desafio01/product.js
@@ -0,0 +1,43 @@
+class TicketManager{    constructor(){
+     this.products = [];
+     this.id = 1;
+ }
+     addProduct(title, description, price, url, code, stock){
+         if ( title && description && price && url && code && stock){
+             const verificationCode = this.products.some (product => product.code === code);
+         if (verificationCode){
+                 console.error("Codigo Repetido");
+             }else{
+                 let id = this.id++;
+                 const newProduct = {id, title, description, price, url, code, stock};
+                 this.products.push(newProduct);
+                 console.log("Producto agregado correctamente:", newProduct);
+             }
+         }else {
+             console.error("Por favor completar todos los campos");        
+         }
+     };
+
+     getProduct(){
+         return this.products;
+     }
+     getProductByID(id){
+         const productID = this.products.find(product => product.id === id);
+         if (!productID){
+             return console.error("Not Found")
+         }else {
+             console.log("El producto solicitado es: ", productID);
+         }
+ }}
+
+
+ const TicketManager = new TicketManager()
+ TicketManager.addProduct("Fideos", "con tuco", 20, "url", 123, 25);
+ TicketManager.addProduct("Ravioles", "de Pollo", 30, "url", 124, 20);
+ TicketManager.addProduct("Ñoquis", "de papa", 40, "url", 125, 50);
+ console.log ("Todos los productos:", TicketManager.getProduct());
+ TicketManager.getProductByID(1);
+ TicketManager.getProductByID(2);
+ TicketManager.getProductByID(3);
+ TicketManager.getProductByID(4);
+ console.log(TicketManager);
